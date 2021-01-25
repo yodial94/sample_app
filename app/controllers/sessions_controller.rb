1492ad85e8a1899d8301f0b26738d113c1b6dbbe
yodial94 <alghiffaari17@gmail.com>
2021-01-25 16:05:53 +0700

    8.2 Logging In: login methode,current user,testing layout changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
+	
   def new
   end
 
   def create
  	user = User.find_by(email: params[:session][:email].downcase)
- 	 if user && user.authenticate(params[:session][:password]) 
- 	# Log the user in and redirect to the user's show page.
+ 	 if user&.authenticate(params[:session][:password]) 
+ 	 	log_in user
+ 	 	redirect_to user
 	 else
 	 	flash.now[:danger] = 'Invalid email/password combination'
 		render 'new'
@@ -13,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+  	log_out
+	redirect_to root_url
   end
 end

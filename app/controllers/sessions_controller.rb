f8747468597def6e73a8cfc60cf3016a616cb1a7
yodial94 <alghiffaari17@gmail.com>
2021-01-25 16:26:10 +0700

    8.3 Log Out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,18 @@
 class SessionsController < ApplicationController
-	
+
   def new
   end
 
   def create
  	user = User.find_by(email: params[:session][:email].downcase)
- 	 if user&.authenticate(params[:session][:password]) 
+ 	if user && user.authenticate(params[:session][:password]) 
+ 		reset_session
  	 	log_in user
  	 	redirect_to user
-	 else
+	else
 	 	flash.now[:danger] = 'Invalid email/password combination'
 		render 'new'
-	 end 
+	end 
   end
 
   def destroy

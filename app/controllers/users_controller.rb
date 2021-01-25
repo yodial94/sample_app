9d8da8c34fe10c1291d8437575a67121c1b5f09f
yodial94 <alghiffaari17@gmail.com>
2021-01-25 14:14:31 +0700

    7.1 Session,login form,finding&autenthication,flash test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,19 +9,18 @@ class UsersController < ApplicationController
   end
 
   def create
- 	@user = User.new(user_params)
+ 	  @user = User.new(user_params)
 		if @user.save
 			flash[:success] = "Welcome to the Sample App!" 
 			redirect_to @user 
 		else
 			render 'new'
-	 end 
-   end
+	  end 
+  end
 
 private
 
   def user_params
- 	params.require(:user).permit(:name, :email, :password,
- 	:password_confirmation)
+ 	  params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  root 'static_pages#home'
-  get  'static_pages/home'
-  get  '/help', to: 'static_pages#help'
-  get  '/about', to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  get  '/news', to: 'static_pages#news'
-  get  '/signup', to: 'users#new'
+  get    'sessions/new'
+  get    'users/new'
+  root   'static_pages#home'
+  get    'static_pages/home'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/news',    to: 'static_pages#news'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end

107ef150793eae59241366a9b11b41d7fd46435a
yodial94 <alghiffaari17@gmail.com>
2021-01-26 15:13:01 +0700

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,19 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
- 	user = User.find_by(email: params[:session][:email].downcase)
- 	if user && user.authenticate(params[:session][:password]) 
- 		reset_session
- 	 	log_in user
- 	 	redirect_to user
-	else
-	 	flash.now[:danger] = 'Invalid email/password combination'
-		render 'new'
-	end 
+ 	    user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+           reset_session
+           log_in user
+           params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+           redirect_to user
+        else
+           flash.now[:danger] = 'Invalid email/password combination'
+           render 'new'
+        end
   end
 
   def destroy
-  	log_out
-	redirect_to root_url
+  	  log_out if logged_in? 
+	    redirect_to root_url
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
@@ -11,12 +12,12 @@ class UserTest < ActiveSupport::TestCase
  end
 
   test "name should be present" do
-  		@user.name = "a" * 51
+  	@user.name = "a" * 51
 		assert_not @user.valid?
  end 
 
   test "email should be present" do
-  		@user.email = "a" * 244 + "@example.com"
+  	@user.email = "a" * 244 + "@example.com"
 		assert_not @user.valid?
  end 
 
@@ -39,22 +40,24 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email addresses should be unique" do
-	duplicate_user = @user.dup
- 	@user.save
-	assert_not duplicate_user.valid?
+	  duplicate_user = @user.dup
+  	@user.save
+	  assert_not duplicate_user.valid?
 	end 
 
-  
-
   test "password should be present (nonblank)" do
- 	@user.password = @user.password_confirmation = " " * 6
- 	assert_not @user.valid?
-    end
+ 	  @user.password = @user.password_confirmation = " " * 6
+ 	  assert_not @user.valid?
+  end
 
   test "password should have a minimum length" do
- 	@user.password = @user.password_confirmation = "a" * 5
- 	assert_not @user.valid?
-    end 
-  end
+ 	  @user.password = @user.password_confirmation = "a" * 5
+ 	  assert_not @user.valid?
+  end 
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end 
+end
 
 
